555266967ac16b4231007873e253e1b0efdc8203
Tiago da Silva Barbosa <tiagoproedu@gmail>
2024-08-22 14:42:01 -0300

    primeiro commit - iniciando projeto

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <nav>
+            <span>Home</span>
+            <span>Portfolio</span>
+            <span>Carreira</span>
+        </nav>
+    )
+}
+
+export default Header;
