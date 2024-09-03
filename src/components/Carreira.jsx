21a755f419f487356618608bed2931c03ef8895f
Tiago da Silva Barbosa <tiagoproedu@gmail.com>
2024-09-03 13:39:23 -0300

    finalizando pagina carreira

--- src/components/Carreira.jsx
+++ src/components/Carreira.jsx
@@ -2,28 +2,30 @@ function Carreira() {
     return (
         <div className="carreira">
             <h1>Carreira</h1>
-            <div className="experiencia">
-                <h2>Experiência</h2>
-                <div className="exp">
-                    <h3>Suporte Técnico | 3A Contabilidade e Assessoria LTDA | 07/2022 - 06/2024</h3>
-                    <ul>
-                        <li>Manutenção de computadores</li>
-                        <li>Suporte ao colaborador</li>
-                        <li>Gerenciamento de servidores</li>
-                    </ul>
+            <div className="expeform">
+                <div className="experiencia">
+                    <h2>Experiência</h2>
+                    <div className="exp">
+                        <h3>Suporte Técnico | 3A Contabilidade e Assessoria LTDA | 07/2022 - 06/2024</h3>
+                        <ul>
+                            <li>Manutenção de computadores</li>
+                            <li>Suporte ao colaborador</li>
+                            <li>Gerenciamento de servidores</li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-            <div className="form-acad">
-                <h2>Formação Acadêmica</h2>
-                <div className="form">
-                    <h3>Análise e Desenvolvimento de Sistemas</h3>
-                    <p><b>Local:</b> Centro Universitário Unifametro</p>
-                    <p><b>Período:</b> 01/2023 - 07/2025</p>
-                </div>
-                <div className="form">
-                    <h3>Desenvolvimento Full Stack</h3>
-                    <p><b>Local:</b> Digital College</p>
-                    <p><b>Período:</b> 06/2023 - 07/2024</p>
+                <div className="form-acad">
+                    <h2>Formação Acadêmica</h2>
+                    <div className="form">
+                        <h3>Análise e Desenvolvimento de Sistemas</h3>
+                        <p><b>Local:</b> Centro Universitário Unifametro</p>
+                        <p><b>Período:</b> 01/2023 - 07/2025</p>
+                    </div>
+                    <div className="form">
+                        <h3>Desenvolvimento Full Stack</h3>
+                        <p><b>Local:</b> Digital College</p>
+                        <p><b>Período:</b> 06/2023 - 07/2024</p>
+                    </div>
                 </div>
             </div>
             <div className="ativ-comp">
